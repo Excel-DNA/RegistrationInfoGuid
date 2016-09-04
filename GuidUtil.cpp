12f150ae057438de7fed5de7b0a0cd43a4b3348f
Tony Roberts <tony@pyxll.com>
2016-09-04 10:37:47 +0100

    Fix return of pointer to stack variable.
    Also, ensure the guid struct is packed correctly, and use wsprinfW for
    printing to wchar_t rather than LPTSTR.

--- GuidUtil.cpp
+++ GuidUtil.cpp
@@ -4,6 +4,29 @@
 
 #define byte unsigned char
 
+#ifdef __GNUC__
+#define PACKED __attribute__((packed))
+#else
+#define PACKED
+#pragma pack(1)
+#endif
+
+typedef struct _dna_guid_t {
+	unsigned __int32 time_low;
+	unsigned __int16 time_mid;
+	unsigned __int16 time_hi_and_version;
+	unsigned __int8  clock_seq_hi_and_reserved;
+	unsigned __int8  clock_seq_low;
+	unsigned __int8  node[6];
+} PACKED dna_guid_t;
+
+dna_guid_t NameSpace_ExcelDna = { /* 306D016E-CCE8-4861-9DA1-51A27CBE341A */
+	0x306D016E,
+	0xCCE8,
+	0x4861,
+	0x9D, 0xA1, 0x51, 0xA2, 0x7C, 0xBE, 0x34, 0x1A,
+};
+
 void SwapBytes(byte* guid, int left, int right)
 {
 	byte temp = guid[left];
@@ -28,7 +51,7 @@ void CopyIntToBytes(int n, byte* pBytes, int offset)
 	pBytes[offset + 3] = n & 0xFF;
 }
 
-byte* CreateGuid(byte* nameSpaceGuid, byte* name, unsigned int nameLength)
+void CreateGuid(byte* nameSpaceGuid, byte* name, unsigned int nameLength, _dna_guid_t* guid)
 {
 	SwapByteOrder(nameSpaceGuid);
 
@@ -40,7 +63,7 @@ byte* CreateGuid(byte* nameSpaceGuid, byte* name, unsigned int nameLength)
 	c.Input(name, nameLength);
 	int resultOK = c.Result(hash);
 
-	byte newGuid[16];
+	byte* newGuid = reinterpret_cast<byte*>(guid);
 	for (int i = 0; i < 4; i++)
 		CopyIntToBytes(hash[i], newGuid, i * 4);
 
@@ -53,26 +76,9 @@ byte* CreateGuid(byte* nameSpaceGuid, byte* name, unsigned int nameLength)
 	
 	// convert the resulting UUID to local byte order
 	SwapByteOrder(newGuid);
-	return (byte*)newGuid;
 }
 
-typedef struct _dna_guid_t {
-	unsigned __int32 time_low;
-	unsigned __int16 time_mid;
-	unsigned __int16 time_hi_and_version;
-	unsigned __int8  clock_seq_hi_and_reserved;
-	unsigned __int8  clock_seq_low;
-	unsigned __int8  node[6];
-} dna_guid_t;
-
-dna_guid_t NameSpace_ExcelDna = { /* 306D016E-CCE8-4861-9DA1-51A27CBE341A */
-	0x306D016E,
-	0xCCE8,
-	0x4861,
-	0x9D, 0xA1, 0x51, 0xA2, 0x7C, 0xBE, 0x34, 0x1A,
-};
-
-dna_guid_t* GuidFromXllPath(const wchar_t* xllPath)
+void GuidFromXllPath(const wchar_t* xllPath, _dna_guid_t* guid)
 {
 	// Convert ToUpper and to UTF8 byte array
 
@@ -91,17 +97,18 @@ dna_guid_t* GuidFromXllPath(const wchar_t* xllPath)
 	int nameLength = WideCharToMultiByte(CP_UTF8, 0, xllPathUpper, -1, name, MAX_LENGTH, NULL, NULL);
 	nameLength--; /* ignore null terminator */
 
-	return (dna_guid_t*)CreateGuid((byte*)&NameSpace_ExcelDna, (byte*)name, nameLength);
+	CreateGuid((byte*)&NameSpace_ExcelDna, (byte*)name, nameLength, guid);
 }
 
 void GuidStringFromXllPath(const wchar_t* xllPath, wchar_t* guidStringBuffer)
 {
-	dna_guid_t* pu = GuidFromXllPath(xllPath);
+	_dna_guid_t guid;
+	GuidFromXllPath(xllPath, &guid);
 
 	// Equivalent of the "N" Guid format in .NET
-	wsprintf(guidStringBuffer, L"%8.8x%4.4x%4.4x%2.2x%2.2x%2.2x%2.2x%2.2x%2.2x%2.2x%2.2x\0",
-		pu->time_low, pu->time_mid,
-		pu->time_hi_and_version, pu->clock_seq_hi_and_reserved,
-		pu->clock_seq_low,
-		pu->node[0], pu->node[1], pu->node[2], pu->node[3], pu->node[4], pu->node[5]);
+	wsprintfW(guidStringBuffer, L"%8.8x%4.4x%4.4x%2.2x%2.2x%2.2x%2.2x%2.2x%2.2x%2.2x%2.2x\0",
+		guid.time_low, guid.time_mid,
+		guid.time_hi_and_version, guid.clock_seq_hi_and_reserved,
+		guid.clock_seq_low,
+		guid.node[0], guid.node[1], guid.node[2], guid.node[3], guid.node[4], guid.node[5]);
 }
